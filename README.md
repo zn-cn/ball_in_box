e89bcbb48fba3e624fc02c146e7f0e5e53a4a59e
molscar <2205567926@qq.com>
2018-05-25 20:51:31 +0800

    init and fix the code of format problem

---
+++ README.md
@@ -0,0 +1,8 @@
+# ball_in_box
+
+
+conda  env create --file environment.yml
+
+conda install --file requirements.txt -y
+
+pip install -e .
---
+++ ball_in_box/ballinbox.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import random
+from ball_in_box.validate import validate
+
+__all__ = ['ball_in_box']
+
+
+def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
+    """
+    m is the number circles.
+    n is the list of coordinates of tiny blocks.
+    This returns a list of tuple, composed of x,y of the circle and r of the circle.
+    """
+
+    # The following is an example implementation.
+    circles = []
+    for circle_index in range(m):
+
+        x = random.random() * 2 - 1
+        y = random.random() * 2 - 1
+        r = random.random() * 0.1
+
+        circles.append((x, y, r))
+        while not validate(circles, blockers):
+            x = random.random() * 2 - 1
+            y = random.random() * 2 - 1
+            r = random.random() * 0.1
+            circles[circle_index] = (x, y, r)
+
+        circle_index += 1
+
+    return circles
